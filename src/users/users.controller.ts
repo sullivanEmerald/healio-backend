dd11cc73dc05d451a0f0417e4a93bd61258aa76d
Sullivan Amadike <amadikesullivan20@gmail.com>
2026-03-09 12:26:17 +0100

    updated users controller

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -2,11 +2,11 @@ import { Controller, Get, Request, UseGuards } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { JwtAuthGuard } from '../auth/guards/jwt-auth.guard';
 
+@UseGuards(JwtAuthGuard)
 @Controller('users')
 export class UsersController {
     constructor(private readonly usersService: UsersService) { }
 
-    @UseGuards(JwtAuthGuard)
     @Get('profile')
     async getProfile(@Request() req: any) {
         const userId = req.user.userId;
